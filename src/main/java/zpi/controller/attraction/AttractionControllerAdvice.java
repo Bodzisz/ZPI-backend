7b58fd7aa859128cee6a541d23d621e2fee47b39
jstankie <jstank4822@gmail.com>
2023-10-24 00:38:57 +0200

    update error handling with @Advice

--- src/main/java/zpi/controller/attraction/AttractionControllerAdvice.java
+++ src/main/java/zpi/controller/attraction/AttractionControllerAdvice.java
@@ -1,5 +1,6 @@
 package zpi.controller.attraction;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,11 @@ public class AttractionControllerAdvice {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         log.error("Invalid argument: ", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        log.error("Entity not found: ", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
